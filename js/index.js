60e95b33dfaa0a2882b295821bd8ba4f3eed729f
dwainejade <dwainejade@gmail.com>
2020-06-09 19:48:55 -0400

    2nd event - change text color

--- js/index.js
+++ js/index.js
@@ -12,13 +12,25 @@
 // const catImage = document.querySelector('.card-img-top');
 // // console.log(catImage);
 
+// zoom busimage
 const busImage = document.querySelector('img');
-console.log('busImage')
 busImage.addEventListener('mouseenter', () => {
     busImage.style.transform = "scale(1.1)";
     busImage.style.transition = "transform 1s";
 });
 busImage.addEventListener('mouseleave', () => {
     busImage.style.transform = 'scale(1)';
+});
+
+
+//click body changes text color
+const click = document.querySelector('body');
+click.addEventListener('click', () => {
+    if(click.style.color === 'green'){
+        click.style.color = 'black';
+    } 
+    else { click.style.color = 'green'
+    }
+
 });
 
